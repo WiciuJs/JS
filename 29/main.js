4b1ad169e762b92d0ab244f9344d2b5bf90f58a4
WiciuJs <witek.janus@int.pl>
2023-01-30 17:28:56 +0100

    poprawione

--- 29/main.js
+++ 29/main.js
@@ -2,7 +2,7 @@ let firstNumber = 11;
 let secondNumber = 9;
 
 function odejmij(liczb, liczb1) {
-    let suma = liczb + liczb1 - 2;
+    let suma = liczb + liczb1;
     return suma;
 }
 console.log(odejmij(7, 5)); 
