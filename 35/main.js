8eed301fb380b49487a84b607fe7b75df2793678
WiciuJs <witek.janus@int.pl>
2023-01-31 22:05:19 +0100

    poprawione

--- 35/main.js
+++ 35/main.js
@@ -1,7 +1,11 @@
-let n = Math.floor(Math.random() * 200);
-if (n > 100 && n <= 200) {
-    console.log(n + " należy do przedziału")
-} else {
-    console.log(n + " nie należy do przedziału ")
 
+function przedzial(n) {
+    if (n > 100 && n <= 200) {
+       return(n + " należy do przedziału")
+    } else {
+        return(n + " nie należy do przedziału ")
+
+    }
 }
+let wynik = przedzial(210);
+console.log(wynik);
