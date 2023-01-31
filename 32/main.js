c0048c4e9425aadf88eab4f1495f8a3c3ec63d2e
WiciuJs <witek.janus@int.pl>
2023-01-31 21:32:06 +0100

    poprawione

--- 32/main.js
+++ 32/main.js
@@ -1,5 +1,12 @@
 let num1 = 2;
 let num2 = 5;
 
-console.log(Math.max(num1 ,num2));
+/* console.log(Math.max(num1 ,num2)); */
   
+	
+function licz (liczb1, liczba2) {
+  let result = Math.max(liczb1 ,liczba2);
+  return result;
+}
+
+console.log(licz(num1,num2));  
