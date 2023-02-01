6f04b8f444fef03fdde530310369c2b8bc9b5fa0
WiciuJs <witek.janus@int.pl>
2023-02-01 23:01:06 +0100

    zadanie

--- 38/main.js
+++ 38/main.js
@@ -1,11 +1,22 @@
-function actions (liczba, liczba1) {
+ function actions (liczba, liczba1) {
+   
    let dodawanie = liczba + liczba1;
    let odejmowanie = liczba - liczba1;
    let mnozenie = liczba * liczba1 ;
-   console.log("Wynik dodawania wynosi = " + dodawanie);
-   console.log("Wynik odejmowania wynosi =  " + odejmowanie);
-   console.log("Wynik mnożenia wynosi = " + mnozenie);
-   
-  }
+   if (odejmowanie < 0 ){
+      console.log("Wynik jest nieprawidłowy"); 
+   }else if (dodawanie < 0 ){
+      console.log("Wynik jest nieprawidłowy"); 
+   }else if (mnozenie < 0 ){
+      console.log("Wynik jest nieprawidłowy"); 
+   }else {
+      console.log("Wynik dodawania wynosi = " + dodawanie);
+      console.log("Wynik odejmowania wynosi =  " + odejmowanie);
+      console.log("Wynik mnożenia wynosi = " + mnozenie);
+   }
   
-  wynik = actions(50, 8);
+  } 
+  
+  actions(50, 51);
+
+
