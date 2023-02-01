e02ed22a102e1c874984ed1b02de4112b0dbd045
WiciuJs <witek.janus@int.pl>
2023-02-01 21:40:52 +0100

    zadanie

---
+++ 38/main.js
@@ -0,0 +1,11 @@
+function actions (liczba, liczba1) {
+   let dodawanie = liczba + liczba1;
+   let odejmowanie = liczba - liczba1;
+   let mnozenie = liczba * liczba1 ;
+   console.log("Wynik dodawania wynosi = " + dodawanie);
+   console.log("Wynik odejmowania wynosi =  " + odejmowanie);
+   console.log("Wynik mnożenia wynosi = " + mnozenie);
+   
+  }
+  
+  wynik = actions(50, 8);
